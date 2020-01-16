1d374b981f0bbea6988945217bb9ecaf98b4be38
K-moovie <kyha97@naver.com>
2020-01-16 22:31:40 +0900

    Revise Signup detail

--- app/src/main/java/dnd/madang/jang/SelfCertificationActivity.kt
+++ app/src/main/java/dnd/madang/jang/SelfCertificationActivity.kt
@@ -1,6 +1,8 @@
 package dnd.madang.jang
 
+import android.content.Intent
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -11,10 +13,18 @@ class SelfCertificationActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selfcertification)
+        val intent = Intent(this, SignupPasswordActivity::class.java)
+
         getNumButton.setOnClickListener {
             showHide(checkNumberTextView)
             showHide(checkNumberButton)
             showHide(checkNumberText)
+        }
+        getPhone.addTextChangedListener(PhoneNumberFormattingTextWatcher()) // -(dash)문자 자동 입력
+
+        checkNumberButton.setOnClickListener {
+            startActivity(intent)
+
         }
     }
 
@@ -25,5 +35,4 @@ class SelfCertificationActivity : AppCompatActivity(){
             View.VISIBLE
         }
     }
-
 }
