bc9d25307fd2045f63d41a924005f388e65a3328
Hoouoo <qwe980813@naver.com>
2020-02-08 18:11:52 +0900

    hi

--- app/src/main/java/dnd/madang/jang/LoginActivity.kt
+++ app/src/main/java/dnd/madang/jang/LoginActivity.kt
@@ -3,7 +3,7 @@ package dnd.madang.jang
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dnd.madang.jang.Findidpw.FindidpwActivity
+import dnd.madang.jang.Findidpw.FindigtActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,7 +12,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login) // 레이아웃 파일이랑 연결하는 코드
         val intent = Intent(this,TermsOfUseActivity::class.java)
-        val intent2 = Intent(this, FindidpwActivity::class.java)
+        val intent2 = Intent(this, FindigtActivity::class.java)
         val intent3 = Intent(this,MainActivity::class.java)
 
         newguestbtn.setOnClickListener{
