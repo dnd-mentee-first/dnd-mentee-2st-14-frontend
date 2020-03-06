c15a9c6f4b200d068902a2f3a23674c1cbc92a5a
SWKANG0525 <stardustsw8947@gmail.com>
2020-03-07 00:06:56 +0900

    Fixed Login Feature

--- app/src/main/java/dnd/madang/jang/RetrofitService.kt
+++ app/src/main/java/dnd/madang/jang/RetrofitService.kt
@@ -7,10 +7,10 @@ interface RetrofitService {
     @GET("search/prdName")
     fun searchProducts(@Query("prd_name")prd_name:String): Call<BaseResponse>
 
-    @Headers("Content-Type:application/x-www-form-urlencoded")
+    @FormUrlEncoded
     @POST("auth/login")
-    fun loginAccount(@Body loginVO : LoginVO): Call<AccountBaseResponse>
+    //fun loginAccount(@Field("user_id")user_id:String,@Field("user_pw")user_pw:String): Call<AccountBaseResponse>
 
-    //fun loginAccount(@Field("user_id") user_id :String,@Field("user_pw")user_pw:String): Call<AccountBaseResponse>
+    fun loginAccount(@Field("user_id") user_id :String,@Field("user_pw")user_pw:String): Call<AccountBaseResponse>
 
 }
